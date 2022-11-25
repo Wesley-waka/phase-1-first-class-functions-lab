7a0a5b194c9fd5a548aaf13ca42bd55880be8756
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-25 17:11:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers= () => {
+ 
+    return drivers.slice(0,2);
+}
+
+console.log(returnFirstTwoDrivers());
+
+const returnLastTwoDrivers=()=> {
+    return drivers.slice(2,4);
+}
+console.log(returnLastTwoDrivers());
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+function createFareMultiplier(integer) {
+    function fare (quad){
+        let quadroupled = integer * quad;
+        return(quadroupled);
+    }
+    return fare;
+}
+
+function fareDoubler(fare){
+    return fare*2
+}
+
+function fareTripler(fare){
+    return fare*3
+}
+function selectDifferentDrivers(arryOfDrivers, returnDrivers){
+    let selectingDrivers = returnDrivers(arryOfDrivers);return selectingDrivers;
+}
+
+
